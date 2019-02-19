fcc3d950d6538c82f790d7bca47f603f28f859dc
juankBregar <juankbregar@gmail.com>
2019-02-19 14:36:46 -0600

    Finished home page

--- js/front.js
+++ js/front.js
@@ -1,5 +1,19 @@
+/**
+ * For changing nav bar background
+ */
 window.onscroll = () => {
     const nav = $('#navbar');
     if (this.scrollY <= 250) nav.removeClass('scrolled');
     else nav.addClass('scrolled');
 };
+
+/**
+ * For rotating cards
+ */
+$('.rot').on('click', function() {
+    $(this).parent().parent().parent().addClass('rotate');
+});
+
+$('.clrot').on('click', function() {
+    $(this).parent().parent().parent().removeClass('rotate');
+});
